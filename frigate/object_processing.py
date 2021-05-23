9c6bd6f2204344abd8916a9c658a0101e4d8152e
Blake Blackshear <blakeb@blakeshome.com>
2021-05-22 21:04:14 -0500

    formatting

--- frigate/object_processing.py
+++ frigate/object_processing.py
@@ -157,24 +157,24 @@ class TrackedObject:
 
     def to_dict(self, include_thumbnail: bool = False):
         event = {
-            'id': self.obj_data['id'],
-            'camera': self.camera,
-            'frame_time': self.obj_data['frame_time'],
-            'label': self.obj_data['label'],
-            'top_score': self.top_score,
-            'false_positive': self.false_positive,
-            'start_time': self.obj_data['start_time'],
-            'end_time': self.obj_data.get('end_time', None),
-            'score': self.obj_data['score'],
-            'box': self.obj_data['box'],
-            'area': self.obj_data['area'],
-            'region': self.obj_data['region'],
-            'current_zones': self.current_zones.copy(),
-            'entered_zones': list(self.entered_zones).copy(),
+            "id": self.obj_data["id"],
+            "camera": self.camera,
+            "frame_time": self.obj_data["frame_time"],
+            "label": self.obj_data["label"],
+            "top_score": self.top_score,
+            "false_positive": self.false_positive,
+            "start_time": self.obj_data["start_time"],
+            "end_time": self.obj_data.get("end_time", None),
+            "score": self.obj_data["score"],
+            "box": self.obj_data["box"],
+            "area": self.obj_data["area"],
+            "region": self.obj_data["region"],
+            "current_zones": self.current_zones.copy(),
+            "entered_zones": list(self.entered_zones).copy(),
         }
-        
+
         if include_thumbnail:
-            event['thumbnail'] = base64.b64encode(self.get_thumbnail()).decode('utf-8')
+            event["thumbnail"] = base64.b64encode(self.get_thumbnail()).decode("utf-8")
 
         return event
 
